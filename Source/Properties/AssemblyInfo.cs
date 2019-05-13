d4cb3447e5709b81f43682d1c1dae0b59feebc64
cake-pie <cake-pie@users.noreply.github.com>
2019-05-13 14:53:43 -0700

    version

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
-[assembly: AssemblyVersion ("0.2.0.0")]
-[assembly: KSPAssembly("unBlur", 0, 2)]
+[assembly: AssemblyVersion ("0.3.0.0")]
+[assembly: KSPAssembly("unBlur", 0, 3)]
